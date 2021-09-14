46a0b6d572faa0b847936593a4d96f7b98231037
henaoerazosebastian995@gmail.com <henaoerazosebastian995@gmail.com>
2021-09-13 22:13:37 -0500

    validar horas iguales

--- crud_clients/functions.py
+++ crud_clients/functions.py
@@ -17,6 +17,9 @@ def valida_tiempo(time_attention,final_attention_time,date_of_request):
         if date_time_obj > datetime.now():
             error = 'La fecha de solicitud no puede ser mayor a la fecha actual , gracias...'
             return error
+        if time_attention == final_attention_time:
+            error = 'La hora final de atencion no puede ser igual a la hora inicial de atencion'
+
     except:
         error = 'El formato de fecha del campo Date of request no es el adecuado recuerde yyyy-mm-dd...'
         return error
